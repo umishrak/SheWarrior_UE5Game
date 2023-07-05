e455e9c016289f26fad5cfd2fdb4dbb94ce4d678
Utpal Kant Mishra <authorutpal@gmail.com>
2023-07-05 09:49:44 +0530

    The Actor Class - Actor and Blueprint created

---
+++ SheWarrior/Source/SheWarrior/SheWarrior.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SheWarrior.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SheWarrior, "SheWarrior" );
